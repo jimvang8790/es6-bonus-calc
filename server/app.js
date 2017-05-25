2dffb7b0071d2d0aea846a9d67634dd57a74ddff
Kris Szafranski <kdszafranski@gmail.com>
2017-05-25 14:03:48 -0500

    udpate to arrow function syntax

--- server/app.js
+++ server/app.js
@@ -9,25 +9,25 @@ var myCompany = new Company('My Company');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/company', function(req, res) {
+app.get('/company', (req, res) => {
   console.log(myCompany);
   res.send(myCompany);
 });
 
-app.get('/process-bonuses', function(req, res) {
+app.get('/process-bonuses', (req, res) => {
   myCompany.calculateBonuses();
   res.status(200).send(myCompany.employees);
 });
 
-app.get('/employees', function(req, res) {
+app.get('/employees', (req, res) => {
   res.send(myCompany.employees);
 });
 
-app.post('/employee', function(req, res) {
+app.post('/employee', (req, res) => {
   myCompany.addEmployee(req.body.name, req.body.startdate, req.body.salary, req.body.reviewRating);
   res.status(201).send(myCompany.employees);
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log('server up on : ', port);
 });
