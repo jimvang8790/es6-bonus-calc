ca4ef0e30a59177f4b34b8420ef1e76b1a59b5a8
Kris Szafranski <kdszafranski@gmail.com>
2017-05-26 15:45:11 -0500

    changed values

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ let Company = require('./modules/company.class');
 
 let myCompany = new Company('My Company');
 
-myCompany.addEmployee('Kris', '12-11-2013', 80000, 4);
-myCompany.addEmployee('Millie', '01-15-2017', 80000, 4);
+myCompany.addEmployee('Kris', '12-11-2013', 40000, 4);
+myCompany.addEmployee('Millie', '01-15-2017', 40000, 4);
 
 myCompany.calculateBonuses();
